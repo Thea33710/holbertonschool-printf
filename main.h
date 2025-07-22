2a6f21619d625469ae98177be527a3ed556e8d8c
Thea33710 <thea33710@gmail.com>
2025-07-22 15:14:34 +0200

    norme betty

--- main.h
+++ main.h
@@ -3,14 +3,21 @@
 
 #include <stdarg.h>
 
+/**
+ * struct spe - Structure pour le specifieur
+ * @specifier: le specifieur
+ * @f: la fonction associee
+ */
+
 typedef struct spe
 {
-  char specifier;
-  int (*f)(va_list *);
+	char specifier;
+	int (*f)(va_list *);
 } spe_t;
 
 int _printf(const char *format, ...);
 int (*get_specifier_func(char c))(va_list *);
+
 int _char(va_list *arg);
 int _string(va_list *arg);
 int _percentage(va_list *arg);
