030362ca8776fa58d7c7ce7f389ebd14e30552f8
Thea33710 <thea33710@gmail.com>
2025-07-22 15:55:45 +0200

    norme betty

--- get_specifier_func.c
+++ get_specifier_func.c
@@ -1,31 +1,33 @@
 #include "main.h"
+#include <stddef.h>
 
-/*
-* get_specifier_func - retrouver la bonne fonction a appeler
-* @c: caractere qui determine le type d'une variable
-* Return: la fonction associee au type d'une variable ou NULL
-*/
+/**
+ * get_specifier_func - recupere le type d'un variable
+ * @c: le caractere qui determine le type d'une variable
+ * Return: la fonction associee au type d'une variable ou NULL
+ */
 
 int (*get_specifier_func(char c))(va_list *)
 {
-  spe_t spec[] = {
-  {'c', _char},
-  {'s', _string},
-  {'%', _percentage},
-  {'\0', NULL}
-};
+	spe_t spec[] = {
+	{ 's', _string },
+	{ 'c', _char },
+	{ '%', _percentage },
+	{ '\0', NULL }
+	};
 
-int i = 0;
+	int i = 0;
 
-/*Boucle tant qu'on n'est pas a la fin du tableau*/
+  /*Boucle tant qu'on n'est pas a la fin du tableau*/
 
-while (spec[i].specifier != '\0')
-{
-    if (spec[i].specifier == c)
-      {
-        return (spec[i].f);
-      }
-      i++;
-}
-return (NULL);
+	while (spec[i].specifier != '\0')
+	{
+		if (spec[i].specifier == c)
+		{
+			return (spec[i].f);
+		}
+		i++;
+	}
+
+	return (NULL);
 }
