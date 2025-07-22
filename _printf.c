63e21c69eea83201b543b40d05842cf76f091dbc
Thea33710 <thea33710@gmail.com>
2025-07-22 11:03:13 +0200

    rectifications

--- _printf.c
+++ _printf.c
@@ -4,23 +4,6 @@
 #include "main.h"
 
 /**
- * _strlen - donne la taille d'une chaine de caracteres
- * @c: chaine de caractere
- * Return: la taille de la chaine de caracteres
- */
-
-int _strlen(char *c)
-{
-int i = 0;
-while (*c != '\0')
-{
-c++;
-i++;
-}
-return (i);
-}
-
-/**
  * _printf - Affiche dans la sortie standard les arguments donnes
  * @format: chaine de caracteres composee d'aucune ou plusieurs directive(s)
  * Return: le nombre total de caracteres imprimes
@@ -29,7 +12,7 @@ return (i);
  int _printf(const char *format, ...)
  {
 	va_list args;
-	int nb_char_printed = 0, nb_char = 0, nb_arg = 2, i, j = 0;
+	int nb_char_printed = 0, nb_char = 0, i, j = 0;
 
 	va_start(args, format);
 
@@ -39,10 +22,8 @@ return (i);
 		{
 			i++;
 			write(1, &format[j], nb_char);
-//			get_specifier_func(format[i])(va_arg(args, format[nb_arg]));
-//			nb_char_printed += (_strlen(format[nb_arg]) - 1);
+			nb_char_printed += get_specifier_func(format[i])(&args);
 			j = i + 1;
-			nb_arg++;
 			nb_char = 0;
 		}
 
@@ -50,7 +31,7 @@ return (i);
 		nb_char++;
 	}
 
-	write(1, &format[j], (nb_char + 1));
+	write(1, &format[j], nb_char);
 	va_end(args);
 	return (nb_char_printed);
  }
