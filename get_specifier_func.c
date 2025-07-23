65a0d1ec0e8c41d50f909e2bae7e5347eb270340
Thea33710 <thea33710@gmail.com>
2025-07-23 11:57:34 +0200

    rajout de cas speciaux

---
+++ get_specifier_func.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * get_specifier_func - recupere le type d'un variable
+ * @c: le caractere qui determine le type d'une variable
+ * Return: la fonction associee au type d'une variable ou NULL
+ */
+
+int (*get_specifier_func(char c))(va_list *)
+{
+	spe_t spec[] = {
+	{ 's', _string },
+	{ 'c', _char },
+	{ '%', _percentage },
+	{ '\0', NULL }
+	};
+
+	int i = 0;
+
+  /*Boucle tant qu'on n'est pas a la fin du tableau*/
+
+	while (spec[i].specifier != '\0')
+	{
+		if (spec[i].specifier == c)
+		{
+			return (spec[i].f);
+		}
+		i++;
+	}
+
+	return (NULL);
+}
